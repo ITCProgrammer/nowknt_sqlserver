ef9239b70ad78a05d827ba60a89c29916fe8387e
usman.as <usmanas.as@gmail.com>
2026-03-10 13:07:20 +0700

    feat: tambahan kolom std dan total demand

--- pages/HasilProduksiHarian.php
+++ pages/HasilProduksiHarian.php
@@ -97,10 +97,40 @@ $Gshift = $Shift;
                 <th valign="middle" style="text-align: center">Berat/Kg</th>
                 <th valign="middle" style="text-align: center">Mesin</th>
                 <th valign="middle" style="text-align: center">No Mesin</th>
+                <th valign="middle" style="text-align: center">Std</th>
+                <th valign="middle" style="text-align: center">Total per DemandNo</th>
+                <th valign="middle" style="text-align: center">Status</th>
               </tr>
             </thead>
             <tbody>
               <?php
+			  $mesinMap = [];
+				$qMesin = sqlsrv_query($con, "SELECT kd_dtex,no_mesin FROM dbknitt.tbl_mesin");
+				while ($m = sqlsrv_fetch_array($qMesin, SQLSRV_FETCH_ASSOC)) {
+					$mesinMap[trim($m['kd_dtex'])] = $m['no_mesin'];
+				}
+			  $stdMap = [];
+
+				$sqlSTD = "
+				SELECT 
+				PRODUCT.SUBCODE02,
+				PRODUCT.SUBCODE03,
+				(ADSTORAGE.VALUEDECIMAL * 24) AS STDRAJUT
+				FROM DB2ADMIN.PRODUCT PRODUCT
+				LEFT JOIN DB2ADMIN.ADSTORAGE ADSTORAGE 
+				ON PRODUCT.ABSUNIQUEID = ADSTORAGE.UNIQUEID
+				WHERE ADSTORAGE.NAMENAME='ProductionRate'
+				AND PRODUCT.ITEMTYPECODE='KGF'
+				AND PRODUCT.COMPANYCODE='100'
+				";
+
+				$stmtSTD = db2_exec($conn1, $sqlSTD);
+
+				while ($r = db2_fetch_assoc($stmtSTD)) {
+					$key = trim($r['SUBCODE02'])."|".trim($r['SUBCODE03']);
+					$stdMap[$key] = round($r['STDRAJUT'],0);
+				}
+	
               $no = 1;
               $c = 0;
               if ($Shift == "1") {
@@ -114,73 +144,127 @@ $Gshift = $Shift;
               } elseif ($Shift == "") {
                 $wkt = " AND SUBSTR(INSPECTIONSTARTDATETIME,1,10) BETWEEN '2021-01-01' AND '2021-01-01' ";
               }
-              $sqlDB21 = " SELECT OPERATORCODE, INITIALS.LONGDESCRIPTION, SUBSTR(INSPECTIONSTARTDATETIME,1,10) AS TGL, SUBSTR(INSPECTIONSTARTDATETIME,12,8) AS JAM, 
-	DEMANDCODE, COUNT(DEMANDCODE) AS JML,SUM(WEIGHTNET) AS KGS,ADSTORAGE.VALUESTRING AS NO_MESIN,SUBSTR(ELEMENTCODE,9,5) AS NROL  
-	FROM ELEMENTSINSPECTION
-LEFT OUTER JOIN DB2ADMIN.INITIALS ON INITIALS.CODE=ELEMENTSINSPECTION.OPERATORCODE	
-LEFT OUTER JOIN DB2ADMIN.PRODUCTIONDEMAND ON PRODUCTIONDEMAND.CODE = ELEMENTSINSPECTION.DEMANDCODE	
-LEFT OUTER JOIN DB2ADMIN.ADSTORAGE ON ADSTORAGE.UNIQUEID = PRODUCTIONDEMAND.ABSUNIQUEID AND ADSTORAGE.NAMENAME ='MachineNo'	
-	WHERE ELEMENTITEMTYPECODE='KGF' $wkt 
-GROUP BY SUBSTR(INSPECTIONSTARTDATETIME,1,10),SUBSTR(INSPECTIONSTARTDATETIME,12,8),DEMANDCODE,OPERATORCODE,INITIALS.LONGDESCRIPTION,ADSTORAGE.VALUESTRING,ELEMENTCODE ";
-              $stmt1   = db2_exec($conn1, $sqlDB21, array('cursor' => DB2_SCROLLABLE));
-              //}	
+		
+              $sqlDB21 = "
+								WITH DATA AS
+				(
+				SELECT 
+					EI.OPERATORCODE,
+					I.LONGDESCRIPTION,
+					SUBSTR(EI.INSPECTIONSTARTDATETIME,1,10) AS TGL,
+					SUBSTR(EI.INSPECTIONSTARTDATETIME,12,8) AS JAM,
+					EI.DEMANDCODE,
+					COUNT(EI.DEMANDCODE) AS JML,
+					SUM(EI.WEIGHTNET) AS KGS,
+					ADS.VALUESTRING AS NO_MESIN,
+					SUBSTR(EI.ELEMENTCODE,9,5) AS NROL,
+
+					KNT.SUBCODE02,
+					KNT.SUBCODE03,
+					KNT.SUBCODE04,
+					KNT.PRODUCTIONORDERCODE,
+					KNT.SUMMARIZEDDESCRIPTION,
+
+					CASE 
+						WHEN KNT.PROJECTCODE <> '' 
+						THEN KNT.PROJECTCODE 
+						ELSE KNT.ORIGDLVSALORDLINESALORDERCODE 
+					END AS PROJECT
+
+				FROM ELEMENTSINSPECTION EI
+
+				LEFT JOIN DB2ADMIN.INITIALS I 
+					ON I.CODE = EI.OPERATORCODE
+
+				LEFT JOIN DB2ADMIN.PRODUCTIONDEMAND PD 
+					ON PD.CODE = EI.DEMANDCODE
+
+				LEFT JOIN DB2ADMIN.ADSTORAGE ADS 
+					ON ADS.UNIQUEID = PD.ABSUNIQUEID 
+					AND ADS.NAMENAME ='MachineNo'
+
+				LEFT JOIN ITXVIEWHEADERKNTORDER KNT 
+					ON KNT.CODE = EI.DEMANDCODE
+					AND KNT.ITEMTYPEAFICODE ='KGF'
+
+				WHERE EI.ELEMENTITEMTYPECODE='KGF'
+				$wkt
+
+				GROUP BY 
+					SUBSTR(EI.INSPECTIONSTARTDATETIME,1,10),
+					SUBSTR(EI.INSPECTIONSTARTDATETIME,12,8),
+					EI.DEMANDCODE,
+					EI.OPERATORCODE,
+					I.LONGDESCRIPTION,
+					ADS.VALUESTRING,
+					EI.ELEMENTCODE,
+					KNT.SUBCODE02,
+					KNT.SUBCODE03,
+					KNT.SUBCODE04,
+					KNT.PRODUCTIONORDERCODE,
+					KNT.SUMMARIZEDDESCRIPTION,
+					KNT.PROJECTCODE,
+					KNT.ORIGDLVSALORDLINESALORDERCODE
+				)
+
+				SELECT 
+					DATA.*,
+					SUM(KGS) OVER(PARTITION BY DEMANDCODE) AS TOTAL_KGS
+				FROM DATA
+				ORDER BY DEMANDCODE, TGL, JAM
+				";
+			  $stmt1   = db2_prepare($conn1, $sqlDB21);
+			  db2_execute($stmt1);	
               $McNo = "";
-              while ($rowdb21 = db2_fetch_assoc($stmt1)) {
-                if (trim($rowdb21['LOGICALWAREHOUSECODE']) == 'M904') {
-                  $knitt = 'LT2';
-                } else if (trim($rowdb21['LOGICALWAREHOUSECODE']) == 'P501') {
-                  $knitt = 'LT1';
-                }
+              while ($row = db2_fetch_assoc($stmt1)) {
+				  
+				    if ($row['JAM'] >= "07.00.00" and $row['JAM'] <= "15.00.00") {
+					  $shf = "1";
+					} elseif ($row['JAM'] >= "15.00.00" and $row['JAM'] <= "23.00.00") {
+					  $shf = "2";
+					} else {
+					  $shf = "3";
+					}
+				  
+					$kdkain = trim($row['SUBCODE02']).trim($row['SUBCODE03'])." ".trim($row['SUBCODE04']);
 
-                $sqlDB22 = " SELECT TRANSACTIONDATE,TRANSACTIONTIME FROM STOCKTRANSACTION WHERE STOCKTRANSACTION.ITEMTYPECODE ='GYR' and (STOCKTRANSACTION.LOGICALWAREHOUSECODE ='P501' OR STOCKTRANSACTION.LOGICALWAREHOUSECODE ='M904') AND
-STOCKTRANSACTION.RETURNTRANSACTION ='1' AND STOCKTRANSACTION.TRANSACTIONDATE='$rowdb21[TRANSACTIONDATE]' 
-AND STOCKTRANSACTION.ORDERCODE='$rowdb21[ORDERCODE]' AND STOCKTRANSACTION.CREATIONUSER='$rowdb21[CREATIONUSER]' ";
-                $stmt2   = db2_exec($conn1, $sqlDB22, array('cursor' => DB2_SCROLLABLE));
-                $rowdb22 = db2_fetch_assoc($stmt2);
-                if ($rowdb21['JAM'] >= "07.00.00" and $rowdb21['JAM'] <= "15.00.00") {
-                  $shf = "1";
-                } elseif ($rowdb21['JAM'] >= "15.00.00" and $rowdb21['JAM'] <= "23.00.00") {
-                  $shf = "2";
-                } else {
-                  $shf = "3";
-                }
-                $sqlDB23 = " SELECT *,CURRENT_TIMESTAMP AS TGLS,
-CASE WHEN PROJECTCODE <> '' THEN PROJECTCODE ELSE ORIGDLVSALORDLINESALORDERCODE  END  AS PROJECT FROM ITXVIEWHEADERKNTORDER WHERE ITEMTYPEAFICODE ='KGF' AND CODE ='" . $rowdb21['DEMANDCODE'] . "' ";
-                $stmt3   = db2_exec($conn1, $sqlDB23, array('cursor' => DB2_SCROLLABLE));
-                $rowdb23 = db2_fetch_assoc($stmt3);
-                $kdkain = trim($rowdb23['SUBCODE02']) . "" . trim($rowdb23['SUBCODE03']) . " " . trim($rowdb23['SUBCODE04']);
-                $McNo = $rowdb21['NO_MESIN'];
-                $sqlKt = sqlsrv_query($con, "SELECT TOP 1 no_mesin FROM dbknitt.tbl_mesin WHERE kd_dtex=?", [$McNo]);
-                $rk = $sqlKt ? sqlsrv_fetch_array($sqlKt, SQLSRV_FETCH_ASSOC) : [];
-                if ($rowdb21['LONGDESCRIPTION'] != "") {
-                  $uid = trim($rowdb21['LONGDESCRIPTION']);
-                } else {
-                  $uid = trim($rowdb21['CREATIONUSER']);
-                }
+					$McNo = trim($row['NO_MESIN']);
 
-              ?>
-                <tr>
-                  <td style="text-align: center"><?php echo $no; ?></td>
-                  <td style="text-align: center"><?php echo $rowdb21['TGL']; ?></td>
-                  <td style="text-align: center"><?php echo $shf; ?></td>
-                  <td style="text-align: center"><?php echo $rowdb21['OPERATORCODE']; ?></td>
-                  <td style="text-align: center"><?php echo $rowdb21['LONGDESCRIPTION']; ?></td>
-                  <td style="text-align: center"><?php echo $rowdb21['DEMANDCODE']; ?></td>
-                  <td style="text-align: center"><?php echo $rowdb23['PROJECT']; ?></td>
-                  <td><?php echo $kdkain; ?></td>
-                  <td style="text-align: center"><?php echo $rowdb23['PRODUCTIONORDERCODE']; ?></td>
-                  <td style="text-align: left"><?php echo $rowdb23['SUMMARIZEDDESCRIPTION']; ?></td>
-                  <td style="text-align: center"><?php echo $rowdb21['JML']; ?></td>
-                  <td style="text-align: center"><?php echo $rowdb21['NROL']; ?></td>
-                  <td style="text-align: right"><?php echo number_format(round($rowdb21['KGS'], 2), 2); ?></td>
-                  <td><?php echo $McNo; ?></td>
-                  <td><?php echo $rk['no_mesin']; ?></td>
-                </tr>
-              <?php
-                $no++;
-              } ?>
-            </tbody>
-
+					$no_mesin = isset($mesinMap[$McNo]) ? $mesinMap[$McNo] : '';
+				  
+				  	$key = trim($row['SUBCODE02'])."|".trim($row['SUBCODE03']);
+				  	$stdrajut = isset($stdMap[$key]) ? $stdMap[$key] : 0;
+				    if($stdrajut > $row['TOTAL_KGS']){
+						$stsKG= "<small class='badge bg-red'>tidak tercapai</small>";
+					}else{
+						$stsKG= "<small class='badge bg-green'>tercapai</small>";
+					}
+				?>
+				<tr>
+				<td><?= $no ?></td>
+				<td><?= $row['TGL'] ?></td>
+				<td><?= $shf ?></td>
+				<td><?= $row['OPERATORCODE'] ?></td>
+				<td><?= $row['LONGDESCRIPTION'] ?></td>
+				<td><?= $row['DEMANDCODE'] ?></td>
+				<td><?= $row['PROJECT'] ?></td>
+				<td><?= $kdkain ?></td>
+				<td><?= $row['PRODUCTIONORDERCODE'] ?></td>
+				<td><?= $row['SUMMARIZEDDESCRIPTION'] ?></td>
+				<td><?= $row['JML'] ?></td>
+				<td><?= $row['NROL'] ?></td>
+				<td align="right"><?= number_format(round($row['KGS'],2),2) ?></td>
+				<td><?= $McNo ?></td>
+				<td><?= $no_mesin ?></td>
+				<td><?= round($stdrajut, 0) ?></td>
+				<td><?= number_format($row['TOTAL_KGS'],2) ?></td>
+				<td><?= $stsKG ?></td>
+				</tr>
+				<?php
+				$no++;
+				}
+				?>
+			  </tbody>
           </table>
         </div>
         <!-- /.card-body -->
